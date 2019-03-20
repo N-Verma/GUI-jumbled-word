50753eb88421e2ff951ab791ae42edfaeab1d5f2
Nikhil Verma <vermaisat@gmail.com>
2019-03-20 16:10:40 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -6,10 +6,11 @@ tv_shows = ['Game of Thrones','Friends','How I Met Your Mother','Breaking Bad','
             'Rick and Morty', 'House of Cards', '13 Reasons Why','House MD', 'Castle','Doctor Who','Dexter','Suits']
 score = 0
 user_list=[]
-jumb_list=[]
 time= 0
-for i in range(1,10,1):
-    user_list.append(random.choice(tv_shows))
+for i in range(1,11,1):
+    choice = random.choice(tv_shows)
+    user_list.append(choice)
+    tv_shows.remove(choice)
 print(user_list)
 
 
@@ -20,72 +21,59 @@ def jumble(word):
         jum +=word[pos]
         word=word[:pos]+word[(pos + 1):]
     return jum
-    
-u=str(user_list[random.randrange(0,9)]) #need to remove later
 
 def sen_jumble(w):
     l=[]
     for i in w:
         new = jumble(i)
-        #print("new=",new)
         l.append(new)
-        #print("l=",l)
     l=" ".join(l)
-    #print(l)
     return l
-#print("Chosen show=>",u)
-#print("Final result=>", sen_jumble(u.split()))
-
-
-
-#define a function
-#loop to make a jumbled list
-#loop to print and match the jumbled word to the gusess made
-#display point and time taken
 
 def start_game(event):
     timetaken()
-    global jumb_list
+    jumb_list = []
     global score
     for i in user_list:
         r1 = sen_jumble(i.split())
         jumb_list.append(r1)
         guess.focus_set()
-    if time>0:
-        for i in range(len(jumb_list)):
-            word.config(text = "Jumbled Word=" + str(jumb_list[i]))
-            if guess.get().lower() == user_list[i].lower():
-                score+=1
-                score_display.config(text = str(score))
-                guess.delete(0,END)
-            else:
-                score+=0
-                score_display.config(text = str(score))
+    word.config(text = "Jumbled Word => " + str(jumb_list[score]))
+    if guess.get().lower() == user_list[score].lower():
+        score += 1
+        score_display.config(text = str(score))
+        try:
+            word.config(text="Jumbled Word => " + str(jumb_list[score]))
+        except IndexError:
+            word.config(text="You Win!")
+            score = 0
+        guess.delete(0,END)
+                
             
 def timetaken():
    global time
    if time>=0:
       time += 1
-      timetaken.config(text = "Time : "+ str(time))
-      timetaken.after(1000, timetaken)
+      timeout.config(text = "Time : "+ str(time))
+      timeout.after(1000, timetaken)
 
 main = Tk()
 main.title("Guess What")
-main.geometry("375x200")
+main.geometry("550x300")
 
-rules = Label(main, text="Guess the correct Tv-show name for the jumbled one shown")
+rules = Label(main, text="Guess the correct Tv-show name for the jumbled one shown",font = ('Times New Roman ',14),fg='Red')
 rules.pack()
 
-word = Label(main)
+word = Label(main,font = ('Times New Roman bold', 14))
 word.pack()
 
-score_display = Label(main)
+score_display = Label(main,font = ('Times New Roman bold', 13))
 score_display.pack()
 
-timeout = Label(main)
+timeout = Label(main,font = ('Times New Roman', 13))
 timeout.pack()
 
-guess = Entry(main)
+guess = Entry(main,font = ('Times New Roman', 13))
 main.bind('<Return>',start_game)
 guess.pack()
 guess.focus_set()
